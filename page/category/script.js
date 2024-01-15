34deb103838f6b1bcb4c3542e054ab63a4d8e36a
bintangsholu21 <bintangsholu@gmail.com>
2024-01-15 14:24:27 +0700

    Update: Category

--- page/category/script.js
+++ page/category/script.js
@@ -1,22 +1,20 @@
-// JavaScript
 async function fetchData() {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
     const data = await response.json();
 
-    const container = document.querySelector('.container');
     data.categories.forEach(category => {
         const card = `
-                <div class="col-md-3 my-3">
-                    <a href="www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}" class="text-decoration-none text-dark">
-                        <div class="card mb-4">
-                            <img src="${category.strCategoryThumb}" class="card-img-top" alt="${category.strCategory}">
-                            <div class="card-body">
-                                <h5 class="card-title">${category.strCategory}</h5>
-                                <p class="card-text">${category.strCategoryDescription}</p>
-                            </div>
-                        </div>
-                    </a>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 my-3 d-flex align-items-center justify-content-center">
+            <a href="" class="text-decoration-none text-dark">
+                <div class="card mb-4">
+                    <img src="${category.strCategoryThumb}" class="card-img-top d-flex align-items-center justify-content-center" alt="${category.strCategory}">
+                    <div class="card-body">
+                        <h5 class="card-title">${category.strCategory}</h5>
+                        <p class="card-text">${category.strCategoryDescription}</p>
+                    </div>
                 </div>
+            </a>
+        </div>
     
         `;
         const row = document.querySelector('.row.categories');
